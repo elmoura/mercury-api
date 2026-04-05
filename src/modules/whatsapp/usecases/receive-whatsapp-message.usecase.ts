624c27641fd0c61a95803dc01fdfc32f39955151
Gabriel Moura <mourasb.gabriel@gmail.com>
2026-04-05 15:17:28 -0300

    checkpoint

--- src/modules/whatsapp/usecases/receive-whatsapp-message.usecase.ts
+++ src/modules/whatsapp/usecases/receive-whatsapp-message.usecase.ts
@@ -5,6 +5,11 @@ export class ReceiveWhatsappMessageUsecase {
   constructor() {}
 
   execute(input: any) {
+    // Recebe mensagem do WhatsApp
+    // Salva mensagem no banco de dados, segmentado pelo orgId e numero que recebeu a mensagem
+    // Envia mensagem para o Mistral LLM para processamento
+    // Envia mensagem de resposta para o WhatsApp
+
     return input;
   }
 }
